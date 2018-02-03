5c40fe54ad81916507bfd2dd09b78bfc76cee9d3
renpengfei <renpengfei@psiot.com.cn>
2018-02-03 15:00:17 +0800

    she's code

--- src/main/java/com/my/blog/website/controller/XiaowanziController.java
+++ src/main/java/com/my/blog/website/controller/XiaowanziController.java
@@ -1,7 +1,14 @@
 package com.my.blog.website.controller;
 
+import com.my.blog.website.service.ILogService;
+import org.apache.catalina.servlet4preview.http.HttpServletRequest;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.stereotype.Service;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 /**
  * 类名称: XiaowanziController
@@ -14,8 +21,18 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class XiaowanziController extends BaseController{
 
+    @Autowired
+    private ILogService logService;
+
     @GetMapping(value = {"xiaowanzi","xiaowanzi.html"})
     public String xiaowanzi(){
         return this.render("xiaowanzi");
+    }
+
+    @RequestMapping(value = "xiaowanziOk")
+    @ResponseBody
+    public void xiaowanziOk(HttpServletRequest request){
+        logService.insertLog("haha","ok",request.getRemoteAddr(),null);
+        System.out.println("is ok");
     }
 }
